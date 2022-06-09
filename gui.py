32358db4b51d7900197a3ef33951a3498c35a1fc
rubenc035 <rubenc035@gmail.com>
2022-06-09 16:32:38 +0200

    Pantalla inicial y selector de archivos

--- gui.py
+++ gui.py
@@ -1,24 +1,51 @@
 import tkinter as tk
-#Esta librería traduce por debajo al lenguaje TCL y lo ejecuta
+from tkinter import ttk
+from tkinter import filedialog
 
-#Creamos una instancia de Tk
-window = tk.Tk()
+class Gui:
+    archivos = []
+    altura_ventana = 0
+    anchura_ventana = 0
+    def __init__(self,altura_ventana,anchura_ventana):
 
-#Definimos la altura y anchura de la ventana
-altura_ventana = 600
-anchura_ventana = 800
+        #Método que lee los archivos y los guarda en la variable
+        def abrirArchivo():
+            self.archivos = filedialog.askopenfilenames(initialdir="/", title="Seleccione archivos", filetypes = (('xlsm files','*.xlsm'), ('xls files','*.xls')))
+            
+        #Creamos una instancia de Tk
+        window = tk.Tk()
 
-#Cogemos el alto y ancho de la pantalla en la que estamos trabajando, le restamos la variable correspondiente
-#y lo dividimos entre 2 con // para obtener un número entero. De esta manera obtendremos la parte que 
-#ha de quedar por cada lado
-altura_pantalla = (window.winfo_screenheight() - altura_ventana) // 2
-anchura_pantalla = (window.winfo_screenwidth() - anchura_ventana) // 2
+        #Definimos la altura y anchura de la ventana
+        self.altura_ventana = altura_ventana
+        self.anchura_ventana = anchura_ventana
 
-#Pasamos primero el tamaño de la ventana y después el lugar en el que queremos colocarla
-window.geometry(f"{anchura_ventana}x{altura_ventana}")
-window.geometry(f"+{anchura_pantalla}+{altura_pantalla}")
+        #Cogemos el alto y ancho de la pantalla en la que estamos trabajando, le restamos la variable correspondiente
+        #y lo dividimos entre 2 con // para obtener un número entero. De esta manera obtendremos la parte que 
+        #ha de quedar por cada lado
+        altura_pantalla = (window.winfo_screenheight() - altura_ventana) // 2
+        anchura_pantalla = (window.winfo_screenwidth() - anchura_ventana) // 2
 
-#Hacemos que tenga un tamaño fijo y que no se pueda agrandar ni empequeñecer
-window.resizable(width=0, height=0)
+        #Pasamos primero el tamaño de la ventana y después el lugar en el que queremos colocarla
+        window.geometry(f"{anchura_ventana}x{altura_ventana}")
+        window.geometry(f"+{anchura_pantalla}+{altura_pantalla}")
 
-window.mainloop()
+        #Hacemos que tenga un tamaño fijo y que no se pueda agrandar ni empequeñecer
+        window.resizable(width=0, height=0)
+
+        # (0,0)    (1,0)    (2,0)
+        # (0,1)    (1,1)    (2,1)
+        # (0,2)    (1,2)    (2,2)
+        # (0,3)    (1,3)    (2,3)
+        # (0,4)    (1,4)    (2,4)
+        window.columnconfigure(0, weight=1)
+        window.columnconfigure(0, weight=4)
+
+        #Creamos una etiqueta que muestra el selector de archivos
+        etqSelFile = ttk.Label(window, text="Seleccionar archivos")
+        etqSelFile.grid(column=0, row=1, sticky=tk.W, padx=5, pady=20)
+
+        btSel = ttk.Button(window, text="", command=abrirArchivo)
+        btSel.grid(column=1, row=1, sticky=tk.W, padx=5, pady=20)
+
+        window.mainloop()
+
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import gui
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from gui import Gui
 
 def main():
-    gui
+    #Creamos una instancia de la Gui y le pasamos el alto y ancho de la ventana
+    ventana = Gui(600,800)
+
+    #Comprobamos los archivos elegidos
+    print(ventana.archivos)
+
 
 if __name__ == '__main__':
     main()
